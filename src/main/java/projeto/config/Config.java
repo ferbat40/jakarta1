9f5822d9f2fe5c48a4792a58821761661d32b2bc
fernando <setec.solda@gmail.com>
2021-12-03 17:22:04 -0300

    dfa

---
+++ src/main/java/projeto/config/Config.java
@@ -0,0 +1,10 @@
+package projeto.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@ApplicationScoped
+public class Config {
+
+}
